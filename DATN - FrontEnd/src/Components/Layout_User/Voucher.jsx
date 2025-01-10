2f1052fca042fb57cfec77075a85c9ac44c2deaa
Lê Anh Thiên <137531393+ThienLeeee@users.noreply.github.com>
2025-01-10 14:28:13 +0700

    Npm i crypto-js

--- DATN - FrontEnd/src/Components/Layout_User/Voucher.jsx
+++ DATN - FrontEnd/src/Components/Layout_User/Voucher.jsx
@@ -3,6 +3,7 @@ import { useAuth } from '../../context/AuthContext';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import CryptoJS from 'crypto-js';
 import { toast } from 'react-toastify';
 import '/public/css/taikhoan.css';
 
